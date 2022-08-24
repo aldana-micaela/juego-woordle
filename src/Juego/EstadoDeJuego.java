4b56190ba057f77a9624ed6ea2675c2ef4d3993b
Juli Nuñez <njulianacamila14@gmail.com>
2022-08-23 23:19:37 -0300

    add metodo elegir palabra

--- src/Juego/EstadoDeJuego.java
+++ src/Juego/EstadoDeJuego.java
@@ -1,6 +1,7 @@
 package Juego;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class EstadoDeJuego {
 	
@@ -15,6 +16,23 @@ public class EstadoDeJuego {
 	private ArrayList<String> palabrasIngresadas;	
 	private int puntaje;
 	private int intentos;
+	public EstadoDeJuego() {
 
+		this.palabra = elegirPalabra();
+		this.palabraSecreta = palabraSecreta;
+		this.letrasAdivinadasEnPosicionCorrecta = new ArrayList<String>();
+		this.letrasEnPosicionIncorrecta = new ArrayList<String>();
+		this.palabrasIngresadas = new ArrayList<String>();
+		this.puntaje = 0;
+		this.intentos = 6;
+	}
+
+
+	private String elegirPalabra() {
+		Random random = new Random();
+		int elem = random.nextInt(this.palabras.length);
+		return this.palabras[elem];
+	}
+	
 
 }
