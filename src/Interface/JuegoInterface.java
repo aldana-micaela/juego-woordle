287db3b6816f8fdbe8521fda46f712b4201c7e29
Juli Nuñez <njulianacamila14@gmail.com>
2022-08-23 21:56:46 -0300

    add cosas a window builder

--- src/Interface/JuegoInterface.java
+++ src/Interface/JuegoInterface.java
@@ -3,10 +3,17 @@ package Interface;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class JuegoInterface {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,8 +43,27 @@ public class JuegoInterface {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setBackground(Color.MAGENTA);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Ingresar palabra:");
+		lblNewLabel.setBounds(45, 24, 97, 24);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(193, 26, 109, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(193, 74, 89, 23);
+		frame.getContentPane().add(btnNewButton);
 	}
 
 }
