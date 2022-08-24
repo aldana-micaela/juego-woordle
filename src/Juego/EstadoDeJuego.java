1822115b3cd96b64fd9eb50008e65aaf8511b258
Juli Nuñez <njulianacamila14@gmail.com>
2022-08-23 23:05:49 -0300

    add lista de palabras

--- src/Juego/EstadoDeJuego.java
+++ src/Juego/EstadoDeJuego.java
@@ -1,7 +1,12 @@
 package Juego;
 
+
+
 public class EstadoDeJuego {
 	
-	
+	private String[] palabras = { "orden", "joven", "botas", "calma", "palma", "jugar", "apodo", "dulce", "vocal",
+			"barco", "regla", "taller", "nadar", "torta", "atomo", "boton", "libro", "cielo", "falso",
+			"carne", "falta", "fuego", "pluma", "tucan", "gatos", "fruta", "poste", "mesas", "motos", "tecla"};
+
 
 }
