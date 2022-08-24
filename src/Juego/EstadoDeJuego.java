c3c39c7b526f52d1ea3ee17782cb2684187aa80e
Juli Nuñez <njulianacamila14@gmail.com>
2022-08-23 23:14:57 -0300

    add variables

--- src/Juego/EstadoDeJuego.java
+++ src/Juego/EstadoDeJuego.java
@@ -1,12 +1,20 @@
 package Juego;
 
-
+import java.util.ArrayList;
 
 public class EstadoDeJuego {
 	
 	private String[] palabras = { "orden", "joven", "botas", "calma", "palma", "jugar", "apodo", "dulce", "vocal",
 			"barco", "regla", "taller", "nadar", "torta", "atomo", "boton", "libro", "cielo", "falso",
 			"carne", "falta", "fuego", "pluma", "tucan", "gatos", "fruta", "poste", "mesas", "motos", "tecla"};
+
+	private String palabra;
+	private char[] palabraSecreta;
+	private ArrayList<String> letrasAdivinadasEnPosicionCorrecta;
+	private ArrayList<String> letrasEnPosicionIncorrecta;
+	private ArrayList<String> palabrasIngresadas;	
+	private int puntaje;
+	private int intentos;
 
 
 }
